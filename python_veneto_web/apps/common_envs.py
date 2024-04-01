4431e15bad3f0e5bfa5d5c7b77ff490b6df1644a
mattia allegro <mattia.allegro@yahoo.it>
2024-04-01 19:52:33 +0200

    (#0000): added BASE_DIR

--- python_veneto_web/apps/common_envs.py
+++ python_veneto_web/apps/common_envs.py
@@ -2,5 +2,6 @@ import os.path
 
 from fastapi.templating import Jinja2Templates
 
-TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "templates/")
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates/")
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
