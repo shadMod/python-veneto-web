a858cb36afefbd464b884fb967d65991b8e40f6c
mattia allegro <mattia.allegro@yahoo.it>
2024-04-13 17:46:04 +0200

    (#0002): added tutorial_category method

--- python_veneto_web/apps/tutorial/views.py
+++ python_veneto_web/apps/tutorial/views.py
@@ -14,6 +14,56 @@ async def tutorial(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="tutorial/index.html",
+        context={
+            "categories": [
+                {
+                    "name": "Basic",
+                    "icon": "icons/tutorial/python.png",
+                },
+                {
+                    "name": "Database",
+                    "icon": "icons/tutorial/database.png",
+                },
+                {
+                    "name": "FrameWork",
+                    "icon": "icons/tutorial/framework.png",
+                },
+                {
+                    "name": "API",
+                    "icon": "icons/tutorial/api.png",
+                },
+                {
+                    "name": "MachineLearning",
+                    "icon": "icons/tutorial/brain.png",
+                },
+                {
+                    "name": "DevOps",
+                    "icon": "icons/tutorial/devops.png",
+                },
+            ]
+        },
+    )
+
+
+@router.get("/tutorial/{category}/", response_class=HTMLResponse)
+async def tutorial_category(request: Request, category: str):
+    tutorial_dir = os.path.join(BASE_DIR, "articles", "tutorial-article", category)
+    filename_list = [
+        filename
+        for filename in os.listdir(tutorial_dir)
+        if os.path.basename(filename).split(".")[1] == "html"
+    ]
+    tutorial_list = []
+    for filename in filename_list:
+        # file_path = os.path.join(tutorial_dir, filename)
+        # tutorial_list.append(get_data_file_path_md(file_path, False))
+        tutorial_list.append(filename)
+    return templates.TemplateResponse(
+        request=request,
+        name="tutorial/list.html",
+        context={
+            "tutorial_list": tutorial_list,
+        },
     )
 
 
