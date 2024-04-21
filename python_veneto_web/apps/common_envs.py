513eafdd75354af68560ac452cbafcad39fa80fe
mattia allegro <mattia.allegro@yahoo.it>
2024-04-21 19:32:33 +0200

    (#0000): rm useless '/'

--- python_veneto_web/apps/common_envs.py
+++ python_veneto_web/apps/common_envs.py
@@ -3,5 +3,5 @@ import os.path
 from fastapi.templating import Jinja2Templates
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-TEMPLATES_DIR = os.path.join(BASE_DIR, "templates/")
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
